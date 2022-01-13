aa95027e02a9ec6e555a0471b11c7c33d6e91128
Ronak Shah <ronakshahultimate@gmail.com>
2022-01-12 23:23:18 -0800

    ready for presentation

--- src/pages/CreateItem/index.tsx
+++ src/pages/CreateItem/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+import { createItem } from '../../api';
+import Button from '../../components/Button';
 import Navbar from '../../components/Navbar';
-import { pathLinks } from '../../pathLinks';
+import TextBox from '../../components/TextBox';
+import { GlobalContext } from '../../contexts/GlobalContext';
+// import { pathLinks } from '../../pathLinks';
 import './style.less';
 
 const CreateItem: React.FC = () => {
+  // const history = useHistory();
+  const { apiUrl } = useContext(GlobalContext);
+  const [name, setName] = useState('');
+
   return (
     <>
       <Navbar title="Create Item" />
       <div className="createItem">
-        <p className="createItem-description">
-          This is how you can make a link <Link to={pathLinks.createAccount}>by using the Link tag from react router</Link>
-        </p>
+        <p className="createItem-description">Add an item to the world’s greatest store</p>
+        <TextBox inputType="text" value={name} label="name" onChange={(t) => setName(t)} />
+
+        <Button
+          text="Create Item"
+          onClick={async () => {
+            await createItem(apiUrl, name, 'description', 0);
+            // history.push(pathLinks.home); // when we're done, go home to see the new item!
+          }}
+        />
       </div>
     </>
   );
